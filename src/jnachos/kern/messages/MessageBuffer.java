a315fbb00083960789fdf13107fc676dbd3af6b3
Himanshu Chhabra <himanshu.chhabra9304@gmail.com>
2018-02-11 21:04:08 -0500

    Edit Comment Line

--- src/jnachos/kern/messages/MessageBuffer.java
+++ src/jnachos/kern/messages/MessageBuffer.java
@@ -2,7 +2,7 @@ package jnachos.kern.messages;
 /**
  * Message buffer, is used as the data structure to keep necessary information for message passing
  * An instance of this class is used every time two new processes want to start communication with each other.
- * Every process maintains a list of instance of this class which it uses for communication.
+ * Every process maintains a list of instance of MessageBuffer class which it uses for communication.
  * A list of instances of this class is maintained by the kernel.
  */
 import java.util.*;
@@ -27,7 +27,7 @@ public class MessageBuffer {
 		this.messages = new ArrayList<String>();
 		isActive = true;
 	}
-	
+
 	public MessageBuffer(NachosProcess sender,NachosProcess receiver,int bufferID) {
 		this.sender = sender;
 		this.receiver = receiver;
@@ -35,27 +35,27 @@ public class MessageBuffer {
 		this.messages = new ArrayList<String>();
 		isActive = true;
 	}
-	
+
 	public NachosProcess getSender() {
 		return this.sender;
 	}
-	
+
 	public NachosProcess getReceiver() {
 		return this.receiver;
 	}
-	
+
 	public int getBufferID() {
 		return this.bufferID;
 	}
-	
+
 	public List<String> getMessages(){
 		return messages;
 	}
-	
+
 	public boolean isActive() {
 		return this.isActive;
 	}
-	
+
 	public void setActiveFlag(boolean flag) {
 		this.isActive = flag;
 	}
